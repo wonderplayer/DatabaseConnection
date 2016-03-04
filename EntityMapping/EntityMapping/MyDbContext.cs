869fc0967924743679e61eca55c943083c7abc8e
Rolands Strakis <roland.strakis@gmail.com>
2016-03-04 09:50:18 +0200

    Track MyDbContext

---
+++ EntityMapping/EntityMapping/MyDbContext.cs
@@ -0,0 +1,32 @@
+﻿using EntityMapping.Configurations;
+using EntityMapping.Entities;
+using System.Data.Entity;
+
+namespace EntityMapping
+{
+    public class MyDbContext : DbContext
+    {
+        static MyDbContext()
+        {
+            Database.SetInitializer<MyDbContext>(null);
+        }
+
+        public MyDbContext() : base("Name=MyDbContext")
+        {
+        }
+
+        public IDbSet<Lists> Lists { get; set; }
+
+        public IDbSet<Fields> Fields { get; set; }
+
+        public IDbSet<Items> Items { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new ListsConfiguration());
+            modelBuilder.Configurations.Add(new FieldsConfiguration());
+            modelBuilder.Configurations.Add(new ItemsConfiguration());
+        }
+    }
+}
